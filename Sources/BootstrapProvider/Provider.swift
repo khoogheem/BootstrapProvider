ce4f504a75e611a9d6287cda4ea73c2fa7a201b2
Kevin A. Hoogheem <kevinh@npe-inc.com>
2017-11-26 09:56:57 -0600

    refactor

--- Sources/BootstrapProvider/Provider.swift
+++ Sources/BootstrapProvider/Provider.swift
@@ -34,6 +34,18 @@ public enum BootstrapColor: String {
     case info
     case light
     case dark
+
+    internal static var allColors: [BootstrapColor] {
+        return [BootstrapColor.primary,
+                BootstrapColor.secondary,
+                BootstrapColor.success,
+                BootstrapColor.danger,
+                BootstrapColor.warning,
+                BootstrapColor.info,
+                BootstrapColor.light,
+                BootstrapColor.dark,
+        ]
+    }
 }
 
 
@@ -63,222 +75,94 @@ public final class Provider: Vapor.Provider {
 
     }
 
-    public func beforeRun(_ droplet: Droplet) throws {
-    }
+    public func beforeRun(_ droplet: Droplet) throws {}
 
-    public init(config: Config) throws {
-    }
+    public init(config: Config) throws {}
 
 }
 
 // MARK: - Buttons
 extension Provider {
 
+    /// Adds in Buttons
+    ///
+    /// - Parameter renderer: LeafRenderer
     private func addButtons(_ renderer: LeafRenderer) {
 
-        ///
-        /// Buttons Standard Size
-        ///
-        renderer.stem.register(Button(size: .standard, color: .primary))
-        renderer.stem.register(Button(size: .standard, color: .secondary))
-        renderer.stem.register(Button(size: .standard, color: .success))
-        renderer.stem.register(Button(size: .standard, color: .danger))
-        renderer.stem.register(Button(size: .standard, color: .warning))
-        renderer.stem.register(Button(size: .standard, color: .info))
-        renderer.stem.register(Button(size: .standard, color: .light))
-        renderer.stem.register(Button(size: .standard, color: .dark))
-        renderer.stem.register(ButtonLink(size: .standard))
-        ///
-        /// Buttons Large Size
-        ///
-        renderer.stem.register(Button(size: .large, color: .primary))
-        renderer.stem.register(Button(size: .large, color: .secondary))
-        renderer.stem.register(Button(size: .large, color: .success))
-        renderer.stem.register(Button(size: .large, color: .danger))
-        renderer.stem.register(Button(size: .large, color: .warning))
-        renderer.stem.register(Button(size: .large, color: .info))
-        renderer.stem.register(Button(size: .large, color: .light))
-        renderer.stem.register(Button(size: .large, color: .dark))
-        renderer.stem.register(ButtonLink(size: .large))
-        ///
-        /// Buttons Small Size
-        ///
-        renderer.stem.register(Button(size: .small, color: .primary))
-        renderer.stem.register(Button(size: .small, color: .secondary))
-        renderer.stem.register(Button(size: .small, color: .success))
-        renderer.stem.register(Button(size: .small, color: .danger))
-        renderer.stem.register(Button(size: .small, color: .warning))
-        renderer.stem.register(Button(size: .small, color: .info))
-        renderer.stem.register(Button(size: .small, color: .light))
-        renderer.stem.register(Button(size: .small, color: .dark))
-        renderer.stem.register(ButtonLink(size: .small))
-        ///
-        /// Button Outline Standard Size
-        ///
-        renderer.stem.register(ButtonOutline(size: .standard, color: .primary))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .secondary))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .success))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .danger))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .warning))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .info))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .light))
-        renderer.stem.register(ButtonOutline(size: .standard, color: .dark))
-        ///
-        /// Button Outline Large Size
-        ///
-        renderer.stem.register(ButtonOutline(size: .large, color: .primary))
-        renderer.stem.register(ButtonOutline(size: .large, color: .secondary))
-        renderer.stem.register(ButtonOutline(size: .large, color: .success))
-        renderer.stem.register(ButtonOutline(size: .large, color: .danger))
-        renderer.stem.register(ButtonOutline(size: .large, color: .warning))
-        renderer.stem.register(ButtonOutline(size: .large, color: .info))
-        renderer.stem.register(ButtonOutline(size: .large, color: .light))
-        renderer.stem.register(ButtonOutline(size: .large, color: .dark))
-        ///
-        /// Button Outline Small Size
-        ///
-        renderer.stem.register(ButtonOutline(size: .small, color: .primary))
-        renderer.stem.register(ButtonOutline(size: .small, color: .secondary))
-        renderer.stem.register(ButtonOutline(size: .small, color: .success))
-        renderer.stem.register(ButtonOutline(size: .small, color: .danger))
-        renderer.stem.register(ButtonOutline(size: .small, color: .warning))
-        renderer.stem.register(ButtonOutline(size: .small, color: .info))
-        renderer.stem.register(ButtonOutline(size: .small, color: .light))
-        renderer.stem.register(ButtonOutline(size: .small, color: .dark))
-
+        for color in BootstrapColor.allColors {
+            /// Buttons Standard Size
+            renderer.stem.register(Button(size: .standard, color: color))
+            /// Buttons Large Size
+            renderer.stem.register(Button(size: .large, color: color))
+            /// Buttons Small Size
+            renderer.stem.register(Button(size: .small, color: color))
+            /// Button Outline Standard Size
+            renderer.stem.register(ButtonOutline(size: .standard, color: color))
+            /// Button Outline Large Size
+            renderer.stem.register(ButtonOutline(size: .large, color: color))
+            /// Button Outline Small Size
+            renderer.stem.register(ButtonOutline(size: .small, color: color))
+        }
     }
 }
 
 // MARK: - Link Button
 extension Provider {
 
+    /// Adds in Link Buttons
+    ///
+    /// - Parameter renderer: LeafRenderer
     private func addLinkButtons(_ renderer: LeafRenderer) {
 
-        ///
-        /// Link Button Standard Size
-        ///
-        renderer.stem.register(LinkButton(size: .standard, color: .primary))
-        renderer.stem.register(LinkButton(size: .standard, color: .secondary))
-        renderer.stem.register(LinkButton(size: .standard, color: .success))
-        renderer.stem.register(LinkButton(size: .standard, color: .danger))
-        renderer.stem.register(LinkButton(size: .standard, color: .warning))
-        renderer.stem.register(LinkButton(size: .standard, color: .info))
-        renderer.stem.register(LinkButton(size: .standard, color: .light))
-        renderer.stem.register(LinkButton(size: .standard, color: .dark))
-        ///
-        /// Link Button Large Size
-        ///
-        renderer.stem.register(LinkButton(size: .large, color: .primary))
-        renderer.stem.register(LinkButton(size: .large, color: .secondary))
-        renderer.stem.register(LinkButton(size: .large, color: .success))
-        renderer.stem.register(LinkButton(size: .large, color: .danger))
-        renderer.stem.register(LinkButton(size: .large, color: .warning))
-        renderer.stem.register(LinkButton(size: .large, color: .info))
-        renderer.stem.register(LinkButton(size: .large, color: .light))
-        renderer.stem.register(LinkButton(size: .large, color: .dark))
-        ///
-        /// Link Button Small Size
-        ///
-        renderer.stem.register(LinkButton(size: .small, color: .primary))
-        renderer.stem.register(LinkButton(size: .small, color: .secondary))
-        renderer.stem.register(LinkButton(size: .small, color: .success))
-        renderer.stem.register(LinkButton(size: .small, color: .danger))
-        renderer.stem.register(LinkButton(size: .small, color: .warning))
-        renderer.stem.register(LinkButton(size: .small, color: .info))
-        renderer.stem.register(LinkButton(size: .small, color: .light))
-        renderer.stem.register(LinkButton(size: .small, color: .dark))
-        ///
-        /// Link Button Outline Standard Size
-        ///
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .primary))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .secondary))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .success))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .danger))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .warning))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .info))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .light))
-        renderer.stem.register(LinkOutlineButton(size: .standard, color: .dark))
-        ///
-        /// Link Button Outline Large Size
-        ///
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .primary))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .secondary))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .success))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .danger))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .warning))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .info))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .light))
-        renderer.stem.register(LinkOutlineButton(size: .large, color: .dark))
-        ///
-        /// Link Button Outline Small Size
-        ///
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .primary))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .secondary))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .success))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .danger))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .warning))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .info))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .light))
-        renderer.stem.register(LinkOutlineButton(size: .small, color: .dark))
+        for color in BootstrapColor.allColors {
+            /// Link Button Standard Size
+            renderer.stem.register(LinkButton(size: .standard, color: color))
+            /// Link Button Large Size
+            renderer.stem.register(LinkButton(size: .large, color: color))
+            /// Link Button Small Size
+            renderer.stem.register(LinkButton(size: .small, color: color))
+            /// Link Button Outline Standard Size
+            renderer.stem.register(LinkOutlineButton(size: .standard, color: color))
+            /// Link Button Outline Large Size
+            renderer.stem.register(LinkOutlineButton(size: .large, color: color))
+            /// Link Button Outline Small Size
+            renderer.stem.register(LinkOutlineButton(size: .small, color: color))
+        }
     }
 }
 
 // MARK: - Alerts
 extension Provider {
 
+    /// Adds in Alerts
+    ///
+    /// - Parameter renderer: LeafRenderer
     private func addAlerts(_ renderer: LeafRenderer) {
 
-        ///
-        /// Alerts
-        ///
-        renderer.stem.register(AlertTag(color: .primary))
-        renderer.stem.register(AlertTag(color: .secondary))
-        renderer.stem.register(AlertTag(color: .success))
-        renderer.stem.register(AlertTag(color: .danger))
-        renderer.stem.register(AlertTag(color: .warning))
-        renderer.stem.register(AlertTag(color: .info))
-        renderer.stem.register(AlertTag(color: .light))
-        renderer.stem.register(AlertTag(color: .dark))
-        ///
-        /// Alerts Dismissable
-        ///
-        renderer.stem.register(AlertTag(color: .primary, dismiss: true))
-        renderer.stem.register(AlertTag(color: .secondary, dismiss: true))
-        renderer.stem.register(AlertTag(color: .success, dismiss: true))
-        renderer.stem.register(AlertTag(color: .danger, dismiss: true))
-        renderer.stem.register(AlertTag(color: .warning, dismiss: true))
-        renderer.stem.register(AlertTag(color: .info, dismiss: true))
-        renderer.stem.register(AlertTag(color: .light, dismiss: true))
-        renderer.stem.register(AlertTag(color: .dark, dismiss: true))
+        for color in BootstrapColor.allColors {
+            /// Add Alerts
+            renderer.stem.register(AlertTag(color: color))
+            /// Add Alerts Dismissable
+            renderer.stem.register(AlertTag(color: color, dismiss: true))
+
+        }
     }
 }
 
 // MARK: - Badges
 extension Provider {
 
+    /// Adds in Badges
+    ///
+    /// - Parameter renderer: LeafRenderer
     private func addBadges(_ renderer: LeafRenderer) {
 
-        ///
-        /// Alerts
-        ///
-        renderer.stem.register(Badge(color: .primary))
-        renderer.stem.register(Badge(color: .secondary))
-        renderer.stem.register(Badge(color: .success))
-        renderer.stem.register(Badge(color: .danger))
-        renderer.stem.register(Badge(color: .warning))
-        renderer.stem.register(Badge(color: .info))
-        renderer.stem.register(Badge(color: .light))
-        renderer.stem.register(Badge(color: .dark))
-        ///
-        /// Alerts Dismissable
-        ///
-        renderer.stem.register(Badge(color: .primary, pill: true))
-        renderer.stem.register(Badge(color: .secondary, pill: true))
-        renderer.stem.register(Badge(color: .success, pill: true))
-        renderer.stem.register(Badge(color: .danger, pill: true))
-        renderer.stem.register(Badge(color: .warning, pill: true))
-        renderer.stem.register(Badge(color: .info, pill: true))
-        renderer.stem.register(Badge(color: .light, pill: true))
-        renderer.stem.register(Badge(color: .dark, pill: true))
+        for color in BootstrapColor.allColors {
+            /// Add Badge
+            renderer.stem.register(Badge(color: color))
+            /// Add Pill Badge
+            renderer.stem.register(Badge(color: color, pill: true))
+
+        }
     }
 }
